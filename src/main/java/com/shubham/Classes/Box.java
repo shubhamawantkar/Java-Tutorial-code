de7df0074d2938104cb79b6d1ef613c998160ae0
Shubham <awantkarshubham99@gmail.com>
2021-12-17 02:28:56 +0530

    Classes

---
+++ src/main/java/com/shubham/Classes/Box.java
@@ -0,0 +1,32 @@
+package com.shubham.Classes;
+
+/*
+A program that uses the box class.
+ */
+public class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+// this class declares an object of type Box.
+
+class BoxDemo{
+    public static void main(String[] args) {
+        Box mybox = new Box();
+        double vol;
+
+        // assign values to mybox's instance variables
+
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
+
+        // compute valume of box
+
+        vol = mybox.width * mybox.height * mybox.depth;
+
+        System.out.println("Volume is "+ vol);
+
+    }
+}
---
+++ src/main/java/com/shubham/Classes/Constructor/ParameterizedConstructor.java
@@ -0,0 +1,43 @@
+package com.shubham.Classes.Constructor;
+/*
+Here, Box uses a parameterized constructor to initialize the dimensions of box.
+
+ */
+public class ParameterizedConstructor {
+    double width;
+    double  height;
+    double depth;
+
+    // This is the constructor for ParameterizedConstructor class
+    ParameterizedConstructor(double w, double h, double d){
+        width = w;
+        height = h;
+        depth = d;
+
+    }
+
+    // compute and return volume
+    double volume(){
+        return width * height * depth;
+    }
+}
+
+class  BoxDemo2{
+    public static void main(String[] args) {
+        // declare, allocate, and initialize ParameterizedConstructor objects
+        ParameterizedConstructor mybox1 = new ParameterizedConstructor(10,20,15);
+        ParameterizedConstructor mybox2 = new ParameterizedConstructor(3,6,9);
+
+        double vol;
+
+        // get volume of first box
+        vol = mybox1.volume();
+        System.out.println("Volume is "+ vol);
+
+        // get volume of second box
+        vol = mybox2.volume();
+        System.out.println("Volume is "+vol);
+
+
+    }
+}
